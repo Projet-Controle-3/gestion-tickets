24f4b4cd2344a7d7ab12e973b9ea66ba7d42e42a
Aymane <aymanee.najibi@gmail.com>
2025-03-18 12:06:00 +0000

    Configuration de la langue avec middleware et gestion dynamique de langue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
+use App\Http\Middleware\LocalizationMiddleware;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+
+Route::middleware(LocalizationMiddleware::class)->group(function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+});
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                   Route pour changer la langue via l'URL                   */
+/* -------------------------------------------------------------------------- */
+Route::get('/locale/{lang}', function ($lang) {
+    session(['lang' => $lang]);
+    return redirect()->back();
+});
+
+
+
+/* -------------------------------------------------------------------------- */
+/*               Route pour changer la langue via un formulaire               */
+/* -------------------------------------------------------------------------- */
+Route::get('/locale', function (Request $request) {
+    $lang = $request->query('lang');
+    session(['lang' => $lang]);
+    return redirect()->back();
 });
