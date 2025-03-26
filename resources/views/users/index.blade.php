a2752c7ed794514657cc91fb536865edd9a5581a
Aymane <aymanee.najibi@gmail.com>
2025-03-26 13:04:14 +0000

    Ajoute Partie de code sans design

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -183,4 +183,42 @@
             </div>
         </div>
     </div>
+
+    {{-- Partie code sans design --}}
+    {{-- <table border="2" style="color: white;">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Nom</th>
+                <th>Email</th>
+                <th>Rôle</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+            <tr>
+                <td>{{$user->id}} </td>
+                <td>{{$user->nom}} </td>
+                <td>{{$user->email}} </td>
+                <td>{{$user->role}}</td>
+                <td class="px-4 py-3 space-x-2 text-center">
+                    <button type="submit"><a href="{{ route('users.edit', $user->id) }}">Modifier</a>
+
+                        <!-- <select name="role" onchange="this.form.submit()">
+                                                <option value="utilisateur" {{ $user->role === 'utilisateur' ? 'selected' : '' }}>Utilisateur</option>
+                                                <option value="admin" {{ $user->role === 'admin' ? 'selected' : '' }}>Admin</option>
+                                                <option value="support" {{ $user->role === 'support' ? 'selected' : '' }}>Support</option>
+                                            </select>
+                                        -->
+                    </button>
+                    <button type="submit"><a href="http://127.0.0.1:8000/delete/{{$user->id}}">supprimer</a></button>
+                </td>
+                @endforeach
+                </td>
+            </tr>
+            <br>
+        </tbody>
+    </table>
+    --}}
 @endsection
